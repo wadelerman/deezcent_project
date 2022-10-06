39705af85ee6d62b2526a5463eafa70267da9036
Eduard Wade Lerman <eduardsnaga@gmail.com>
2022-10-07 04:49:37 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Deezcent());
+}
+
+class Deezcent extends StatefulWidget {
+  const Deezcent({super.key});
+
+  @override
+  State<Deezcent> createState() => _DeezcentState();
+}
+
+class _DeezcentState extends State<Deezcent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
