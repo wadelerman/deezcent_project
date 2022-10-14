411e86f9cd40216db48bed4353011ebffea2ff42
Eduard Wade Lerman <eduardsnaga@gmail.com>
2022-10-14 12:22:36 +0800

    making login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const Deezcent());
 }
 
-class Deezcent extends StatefulWidget {
+class Deezcent extends StatelessWidget {
   const Deezcent({super.key});
 
   @override
-  State<Deezcent> createState() => _DeezcentState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Testim',
+      theme: ThemeData(fontFamily: 'Circular'),
+      home: const HomePage(),
+    );
+  }
 }
 
-class _DeezcentState extends State<Deezcent> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:  AppBar(title: const Text('Testing font ciruclar')),
+      body: const Center(
+        child: Text('lmaooooooooooooooooooo')
+      ),
+    )
   }
 }
