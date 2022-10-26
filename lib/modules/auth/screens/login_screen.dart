90bba3491f305297186c72e589dd5bc2e61ba40d
Eduard Wade Lerman <eduardsnaga@gmail.com>
2022-10-27 03:12:26 +0800

    added loading screen while login

--- lib/modules/auth/screens/login_screen.dart
+++ lib/modules/auth/screens/login_screen.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:deezcent_project/common/button_widget.dart';
+
+import '../../../shared/loading.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,14 +14,27 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginState extends State<LoginScreen> {
   //text controller
+  // final AuthService _auth = AuthService();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool loading = false;
 
   Future signIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: _emailController.text.trim(),
-      password: _passwordController.text.trim(),
-    );
+    try {
+      setState(() {
+        loading = true;
+      });
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+    } catch (error) {
+      print(error.toString());
+      setState(() {
+        loading = false;
+      });
+      return null;
+    }
   }
 
 //memory management
@@ -33,117 +47,96 @@ class _LoginState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: SafeArea(
-          child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.flash_on_outlined,
-                size: 75,
-                color: Colors.teal[600],
-              ),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.grey[200],
+            body: SafeArea(
+                child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.flash_on_outlined,
+                      size: 75,
+                      color: Colors.teal[600],
+                    ),
 
-              SizedBox(height: 20),
-              Text(
-                'Welcome back!',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 10),
-              //Top Subtitle text
-              Center(
-                child: Text(
-                  'Silahkan isi email dan password untuk login',
-                  style: TextStyle(fontWeight: FontWeight.w200, fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-
-              //Login innput box
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      // border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email',
+                    SizedBox(height: 20),
+                    Text(
+                      'Welcome back!',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                    //Top Subtitle text
+                    Center(
+                      child: Text(
+                        'Silahkan isi email dan password untuk login',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w200, fontSize: 20),
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                  ),
-                ),
-              ),
 
-              //password input box
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      // border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
+                    //Login innput box
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            // border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                          child: TextField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Email',
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+
+                    //password input box
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            // border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                          child: TextField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Password',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    //login button
+                    SizedBox(height: 20),
+                    loginButton(),
+                    //forgot password text
+                    SizedBox(height: 20),
+                    Center(child: Text('Lupa Password?')),
+                  ],
                 ),
               ),
-
-              //login button
-              SizedBox(height: 20),
-              // Padding(
-              //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              //   child: GestureDetector(
-              //     onTap: signIn,
-              //     child: Container(
-              //       padding: EdgeInsets.all(15),
-              //       decoration: BoxDecoration(
-              //         color: Colors.teal[800],
-              //         borderRadius: BorderRadius.circular(15),
-              //       ),
-              //       child: Center(
-              //         child: Text('Login',
-              //             style: TextStyle(
-              //               fontSize: 17,
-              //               fontWeight: FontWeight.w600,
-              //               color: Colors.white,
-              //             )),
-              //       ),
-              //     ),
-              //   ),
-              // ),
-
-              SizedBox(height: 20),
-              loginButton(),
-              // GestureDetector(onTap: signIn, child: loginButton()),
-
-              //forgot password text
-              SizedBox(height: 20),
-              Center(child: Text('Lupa Password?')),
-            ],
-          ),
-        ),
-      )),
-    );
+            )),
+          );
   }
 
   Widget loginButton() => Builder(
